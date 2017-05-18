adb8e1564f124a466918b200aaf8df8c439c6ce2
gys <gysly@gmail.com>
2017-05-18 14:26:07 +0800

    commit

--- sellELM/src/App.vue
+++ sellELM/src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <div class="header">
+    I am header
+    </div>
+    <div class="tab">
+    I am tab
+    </div>
+    <div class="content">
+    I am content
+    </div>
+
+  <!--  <img src="./assets/logo.png">
+    <router-view></router-view>  --> 
   </div>
 </template>
 
---
+++ sellELM/src/components/header/header.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script type="text/ecmascript-6">
+    
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    
+
+</style>
+
+<template> 
+    
+</template>
+<script type = 'text/ecmascript-6' >
+</script>
+<style lang = 'stylus' rel = 'stylesheet/stylus' >
+</style>
